2d09582bf64ce757d20c194cac139c479906e08e
admin-402 <92362676+admin-402@users.noreply.github.com>
2022-07-21 19:02:49 +0530

    portfolio-2022

---
+++ portfolio/pages/document.tsx
@@ -0,0 +1,14 @@
+import {Html, Head, Main, NextScript} from 'next/document'
+
+export default function Document() {
+  return (
+    <html className='dark'>
+      <head>
+      </head>
+      <body>
+        <main/>
+        <NextScript/>
+      </body>
+    </html>
+  )
+}
